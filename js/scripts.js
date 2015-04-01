62ae13731d7b4995a0c9ef224724d05e15273b7b
jacobnisnevich <jacob.nisnevich@gmail.com>
2015-03-31 23:33:56 -0700

    added card backs

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
 		$('.active').removeClass('active');
 		$(this).addClass('active');
 	});
+
+	$('.portfolio-item').click(function() {
+		$(this).find('.front').animate({opacity:($(this).find('.front').css('opacity')==1)?0:1});	});
 });
